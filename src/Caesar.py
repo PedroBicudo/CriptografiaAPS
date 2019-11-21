d368f7e09303bcc48ee3faf8ad7c364ece69e323
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2019-11-21 20:25:34 -0300

    Adicionados raises para verificacoes especificas.

--- src/Caesar.py
+++ src/Caesar.py
@@ -1,19 +1,11 @@
 """Cifra de cesar."""
+from exceptions import SizeStringError
 from string import ascii_lowercase
 from operator import add, sub
 
 
 def getRotLetter(letter, rot, operation, alphabet):
     """Obter a letra rotacionada.
-
-    Description:
-        Sera verificado se a letra existe no alfabeto. Caso
-        nao exista, a propria letra sera retornada. Caso contrario
-        sua posicao sera mapeada no alfabeto e a nova letra 
-        sera buscada a partir de uma operacao fornecida metodo 
-        anterior __caesar__ e o resultado dessa operacao estara 
-        sujeita ao modulo do tamanho do alfabeto para evitar 
-        IndexError.
 
     Arguments:
         letter {str} -- Letra a ser rotacionada.
@@ -25,23 +17,31 @@ def getRotLetter(letter, rot, operation, alphabet):
         str -- Letra cifrada.
 
     """
+    if not isinstance(letter, str) or not isinstance(alphabet, str):
+        raise TypeError("'letter' e 'alphabet' devem ser do tipo string.")
+    
+    if len(letter) > 1 or not letter:
+        raise SizeStringError("'letter' deve ter length 1.")
+
+    if not isinstance(rot, int):
+        raise TypeError("'rot' deve ser do tipo integer.")
+
+    if not callable(operation):
+        raise TypeError("'operation' deve ser uma funcao.")
+
+
     if letter not in alphabet:
         result = letter
+
     else:
         pos_letter = alphabet.index(letter)
         result = alphabet[operation(pos_letter, rot) % len(alphabet)]
     return result
 
 
-def __caesar__(msg, rot, operation, alphabet):
+def caesar(msg, rot, operation, alphabet=ascii_lowercase):
     """Criptografar/descriptografar a cifra.
 
-    Description:
-        Vai mapear todas as letras em 'msg', inserindo no
-        metodo getRotLetter com os atributos fornecidos pelo
-        metodo __caesar__. Em seguida todos os valores serao
-        concatenados e retornados ao usuario.
-    
     Arguments:
         msg {str} -- Mensagem.
         rot {int} -- Valor de rotacao da cifra.
@@ -70,7 +70,7 @@ def encript(msg, rot, alphabet=ascii_lowercase):
         str -- Mensagem criptografada.
 
     """
-    return __caesar__(msg, rot, add, alphabet)
+    return caesar(msg, rot, add, alphabet)
 
 
 def decript(msg, rot, alphabet=ascii_lowercase):
@@ -87,30 +87,4 @@ def decript(msg, rot, alphabet=ascii_lowercase):
         str -- Mensagem descriptografada.
 
     """
-    return __caesar__(msg, rot, sub, alphabet)
-    
-
-if __name__ == "__main__":
-    from string import ascii_uppercase, ascii_letters
-    ##
-    ## CASO O CARACTER NAO ESTEJA NO ALFABETO 
-    ## ELE MESMO SERA RETORNADO!
-    ##
-
-    # Exemplo com alfabeto lowercase
-    new_letter = encript('teste', 12)
-    print(new_letter)
-    org_letter = decript(new_letter, 12)
-    print(org_letter, end='\n\n')
-
-    # Exemplo com alfabeto UpperCase
-    new_letter = encript('ALGUMA COISA', 55, ascii_uppercase)
-    org_letter = decript(new_letter, 55, ascii_uppercase)
-    print(new_letter)
-    print(org_letter, end='\n\n')
-
-    # Exemplo com ambos os tipos
-    new_letter = encript('Alguma Coisa123', 125, ascii_letters)
-    org_letter = decript(new_letter, 125, ascii_letters)
-    print(new_letter)
-    print(org_letter, end='\n\n')
+    return caesar(msg, rot, sub, alphabet)
