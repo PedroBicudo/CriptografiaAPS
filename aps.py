fe169d115e7a92fbc7c9f95b3db9ad471743f448
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-01-17 17:43:01 -0300

    Parametros de encriptar e desencriptar adicionados.

--- aps.py
+++ aps.py
@@ -28,3 +28,19 @@ cript_manager.add_argument(
     type=argparse.FileType('w', encoding="utf-8"),
     dest="output"
     )
+
+# Acoes para a criptografia
+action_crip = required_args.add_mutually_exclusive_group(required=True)
+action_crip.add_argument(
+    "--encript", "-e",
+    help="Encriptar a mensagem.",
+    action="store_true",
+    dest='action'
+    )
+
+action_crip.add_argument(
+    "--decript", "-d",
+    help="Desencriptar a mensagem.",
+    action="store_false",
+    dest='action'
+    )
