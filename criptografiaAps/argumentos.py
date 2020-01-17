1b8e74eefd28db3c8bbfff157d73ab41f30e934d
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-01-17 17:47:51 -0300

    Adicionado metodo de saida de informacoes.

---
+++ criptografiaAps/argumentos.py
@@ -0,0 +1,20 @@
+"""Direcionar os parametros argparse para as respectivas criptografias."""
+
+def _output_format(msg, output):
+    """Formato de saida da mensagem criptografada/descriptografada.
+
+    Parameters
+    ----------
+    msg: str
+        Mensagem a ser visualizada.
+
+    output: [None, _io.TextIOWrapper]
+        Tipo de saida.
+
+    """
+    if isinstance(output, _io.TextIOWrapper):
+        output.write(msg)
+        output.close()
+
+    else:
+        print(f"Text: {msg}")
