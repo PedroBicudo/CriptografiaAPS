cf17346cf769a88ff09fa9fd6e3ae7c7b8dad4be
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-01-17 17:14:47 -0300

    Sistema de arquivos removido.

--- src/exceptions.py
+++
@@ -1,7 +0,0 @@
-"""Arquivos contendo as exceções do projeto."""
-
-class CriptException(BaseException):
-    """Excecao base para o projeto."""
-
-class SizeStringError(CriptException):
-    """Erro de tamanho de string."""
