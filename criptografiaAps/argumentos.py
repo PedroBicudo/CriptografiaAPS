d15abb403a507f68e4511c479ca47e7404d4486f
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-01-17 17:52:41 -0300

    Adicionado metodo de escolha de criptografias.

--- criptografiaAps/argumentos.py
+++ criptografiaAps/argumentos.py
@@ -1,6 +1,8 @@
 """Direcionar os parametros argparse para as respectivas criptografias."""
 import _io
 
+__all__ = []
+
 def _output_format(msg, output):
     """Formato de saida da mensagem criptografada/descriptografada.
 
@@ -19,4 +21,33 @@ def _output_format(msg, output):
 
     else:
         print(f"Text: {msg}")
+
+def _get_cript_action(cript, action):
+    """Escolher a criptografia e a acao a ser efetuada.
+
+    Parameters
+    ----------
+    cript: str
+        Nome da criptografia
+
+    action: bool
+        Encriptar/Desencriptar (True or False)
+
+    Raises
+    ----------
+    NotImplementedError
+        A criptografia especificada nao foi implementada.
+
+    Returns
+    ----------
+    function|None
+        Funcao representando acao na criptografia ou None, como caso
+        especifico.
+
+        Existem tres tipos de saida possiveis, sendo elas criptografia,
+        descriptografia e geracao de chaves para a criptografia RSA.
+
+    """
+    raise NotImplementedError
+
 
