dca6f23ae1c992430982908febf2375eae9d01ca
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-10 12:07:15 -0300

    Metodo de rotacionamento de letras adicionado.

--- criptografiaAps/caesar.py
+++ criptografiaAps/caesar.py
@@ -3,6 +3,31 @@ from criptografiaAps.AbstractCriptModel import AbstractCriptModel
 
 
 class Caesar(AbstractCriptModel):
+
+    def _get_rot_letter(self, letter, rot, operation, alphabet)
+        """Obter a letra rotacionada.
+
+        Parameters
+        ----------
+        letter: str
+            Letra a ser rotacionada.
+
+        rot: int
+            Valor de rotacao da cifra.
+
+        operation: function
+            Tipo de operacao.
+
+        alphabet: str
+            Alfabeto a ser usado.
+
+        Returns
+        ----------
+        str
+            Letra cifrada.
+
+        """
+
     
     def encript(self, text, rot):
         ...
