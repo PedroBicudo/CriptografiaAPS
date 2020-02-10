0aed2ab26099b11ccb489c646b6f56730a36a417
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-10 11:45:12 -0300

    Classe Caesar criada.

--- criptografiaAps/caesar.py
+++ criptografiaAps/caesar.py
@@ -1,1 +1,11 @@
 """Criptografia Cifra de César."""
+from criptografiaAps.AbstractCriptModel import AbstractCriptModel
+
+
+class Caesar(AbstractCriptModel):
+    
+    def encript(self, text, rot):
+        ...
+
+    def decript(self, enc_text, rot):
+        ...
