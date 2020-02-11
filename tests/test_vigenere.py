a3defb36db4e8ba0d7f83a36b64211a217e073a7
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-11 14:48:52 -0300

    Testes de obtenção de letras adicionados.

--- tests/test_vigenere.py
+++ tests/test_vigenere.py
@@ -1,6 +1,19 @@
 from criptografiaAps import Vigenere
+from string import ascii_lowercase
+from operator import add, sub
 import unittest
 
 class VigenereTest(unittest.TestCase):
     def setUp(self):
         self.vigenere = Vigenere()
+    
+    def test_letter_getter(self):
+        self.assertEqual(
+            self.vigenere._get_letter(1, 10, add, ascii_lowercase), 
+            'l'
+        )
+
+        self.assertEqual(
+            self.vigenere._get_letter(1, 10, sub,  ascii_lowercase),
+            'r'
+        )
