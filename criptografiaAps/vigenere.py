9607cfbc8c97c51896445910b9cc626f3275b072
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-11 14:15:41 -0300

    Método de obtenção de palavras adicionado.

--- criptografiaAps/vigenere.py
+++ criptografiaAps/vigenere.py
@@ -26,3 +26,35 @@ class Vigenere(AbstractCriptModel):
 
         """
         return alphabet[operator(letter_pos, key_pos) % len(alphabet)]
+
+    def _get_word(self, msg, key, operator, alphabet):
+        """Obter a palavra criptografada/descriptografada.
+
+        Parameters
+        ----------
+        msg: str
+            Mensagem qualquer.
+
+        key: str
+            Chave para encriptacao/decriptacao.
+
+        operator: function
+            Operacao a ser realizada.
+
+        alphabet: str
+            Alfabeto a ser usado.
+
+        Returns
+        ----------
+        str
+            Texto encriptado/decriptado.
+        """
+        new_msg = ""
+        for index, letter in enumerate(msg):
+            letter_pos = alphabet.index(letter)
+            key_pos = alphabet.index(key[index])
+            new_msg += self._get_letter(
+                letter_pos, key_pos,
+                operator, alphabet
+                )
+        return new_msg
