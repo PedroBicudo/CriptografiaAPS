9b6f8512780b045a8bab94b235f79d4e95bbd3e4
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-11 14:12:05 -0300

    Metodo de obtenção de letras adicionado.

--- criptografiaAps/vigenere.py
+++ criptografiaAps/vigenere.py
@@ -2,4 +2,27 @@ from criptografiaAps import AbstractCriptModel
 
 class Vigenere(AbstractCriptModel):
     
+    def _get_letter(self, letter_pos, key_pos, operator, alphabet):
+        """Obter a nova letra apos a encriptacao/desencriptacao.
 
+        Parameters
+        ----------
+        letter_pos: int
+            Posicao da letra.
+
+        key_pos: int
+            Posicao da chave.
+
+        operator: function
+            Operacao a ser realizada.
+
+        alphabet: str
+            Alfabeto a ser usado.
+
+        Returns
+        ----------
+        str
+            Letra do alfabeto.
+
+        """
+        return alphabet[operator(letter_pos, key_pos) % len(alphabet)]
