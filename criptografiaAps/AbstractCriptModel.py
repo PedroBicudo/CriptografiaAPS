b1db21e67640a32fe1e3a80385609acd942e921d
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-13 13:41:43 -0300

    Docstrings adicionadas.

--- criptografiaAps/AbstractCriptModel.py
+++ criptografiaAps/AbstractCriptModel.py
@@ -1,15 +1,32 @@
+"""Classe de abstração de criptografias."""
 import abc
 
 class AbstractCriptModel(abc.ABC):
+    """AbstractCriptModel."""
 
     def get_action(self, action):
+        """Obter ação da criptografia.
+        
+        Esse método é utilizado pelo arquivo argumentos.
+        
+        Parameters
+        ----------
+        action: bool
+            Valor boolean para obter ação de criptografia ou descriptografia. 
+        
+        Returns
+        ----------
+        method
+            Método representando a ação a ser realizada.
+        """
         return self.encript if action else self.decript
 
     @abc.abstractmethod
     def encript(self, *args, **kwargs):
+        """Encriptar mensagem."""
         raise NotImplementedError
 
     @abc.abstractmethod
     def decript(self, *args, **kwargs):
+        """Desencriptar mensagem."""
         raise NotImplementedError
-
