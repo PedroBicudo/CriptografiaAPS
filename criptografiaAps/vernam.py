9b202da672b71a5cb9550f9a77750067193f4948
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-22 13:59:09 -0300

    Adicionado metodo _hex_to_chr.

--- criptografiaAps/vernam.py
+++ criptografiaAps/vernam.py
@@ -23,3 +23,24 @@ class Vernam(AbstractCriptModel):
             raise ValueError("Apenas um hexadecimal deve ser inserido.")
 
         return set(value.split('0x')[-1]) <= set(hexdigits)
+    
+    def _hex_to_chr(self, msg_hex, sep=':'):
+        """Realizar conversao da mensagem em hexadecimal para Caractere ASCII.
+
+        Parameters
+        ----------
+        msg_hex: str
+            Mensagem em hexadecimal
+
+        sep: str
+            Delimitador de hexadecimal {default: ':'}.
+
+        Returns
+        ----------
+        list
+            Lista contendo cada valor hexadecimal traduzido para o seu
+            respectivo caractere na tabela ASCII.
+
+        """
+        msg_dec = [int(value_hex, base=16) for value_hex in msg_hex.split(sep)]
+        return [chr(value_dec) for value_dec in msg_dec]
