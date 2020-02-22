48749e801b0e769772dea09100293f4ac2267e20
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-22 13:15:03 -0300

    Testes do metodo _is_hex adicionados.

--- tests/test_vernam.py
+++ tests/test_vernam.py
@@ -6,3 +6,29 @@ class VernamTest(unittest.TestCase):
     
     def setUp(self):
         self.vernam = Vernam()
+
+    def test_is_hex(self):
+        self.assertTrue(
+            self.vernam._is_hex('0xFF')
+        )
+
+        self.assertTrue(
+            self.vernam._is_hex('FF')
+        )
+        
+        self.assertFalse(
+            self.vernam._is_hex('0xZZ')
+        )
+
+        self.assertFalse(
+            self.vernam._is_hex('ZZ')
+        )
+
+        # Raises
+        with self.assertRaises(ValueError) as error:
+            self.vernam._is_hex('0x440xAA')
+        
+        self.assertTrue(
+            'Apenas um hexadecimal deve ser inserido.'
+            in str(error.exception)
+            )
