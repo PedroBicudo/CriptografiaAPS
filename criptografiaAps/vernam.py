35ef5fd0040d81a1cbc59dd6c87fd66047a6b658
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-22 13:13:17 -0300

    Metodo de verificação hexadecimal adicionado.

--- criptografiaAps/vernam.py
+++ criptografiaAps/vernam.py
@@ -1,5 +1,25 @@
 """Cifra de Vernam."""
 from criptografiaAps import AbstractCriptModel
+from string import hexdigits
 
 class Vernam(AbstractCriptModel):
-    ...
+
+
+    def _is_hex(self, value):
+        """Verifica se é um hexadecimal.
+
+        Parameters
+        -----------
+        value: str
+            Possível valor em hexadecimal.
+        
+        Returns
+        -----------
+        bool
+
+        """
+        value = value.replace('0X', '0x')
+        if value.count('0x') > 1:
+            raise ValueError("Apenas um hexadecimal deve ser inserido.")
+
+        return set(value.split('0x')[-1]) <= set(hexdigits)
