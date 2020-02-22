a6083fd5c2246f13e3326a3cfefeec6d5133c635
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-02-22 14:29:11 -0300

    Adicionados testes dos metodos: _vernam, encript, decript.

--- tests/test_vernam.py
+++ tests/test_vernam.py
@@ -36,5 +36,49 @@ class VernamTest(unittest.TestCase):
     def test_hex_to_char(self):
         self.assertEqual(
             self.vernam._hex_to_chr('0x74:0x65:0x73:0x74:0x65'),
+            list('teste')
+        )
+    
+    def test_vernam(self):
+        self.assertEqual(
+            self.vernam._vernam('teste', 'abcde'),
+            [21, 7, 16, 16, 0]
+        )
+
+        # Raises
+        with self.assertRaises(TypeError) as error_msg:
+            self.vernam._vernam(1234, 'teste')
+        
+        with self.assertRaises(TypeError) as error_key:
+            self.vernam._vernam('teste', 12345)
+
+        with self.assertRaises(ValueError) as error_size:
+            self.vernam._vernam('teste', 'test')
+
+        self.assertTrue(
+            "'msg' devem ser do tipo string ou list."
+            in str(error_msg.exception)
+        )
+
+        self.assertTrue(
+            "key' devem ser do tipo string ou list."
+            in str(error_key.exception)
+        )
+
+        self.assertTrue(
+            "'key' deve ter tamanho maior ou igual a 'msg'."
+            in str(error_size.exception)
+        )
+
+
+    def test_encript(self):
+        self.assertEqual(
+            self.vernam.encript('teste', 'abcde'),
+            '0x15:0x7:0x10:0x10:0x0'
+        )
+
+    def test_decript(self):
+        self.assertEqual(
+            self.vernam.decript('0x15:0x7:0x10:0x10:0x0', 'abcde'),
             'teste'
         )
